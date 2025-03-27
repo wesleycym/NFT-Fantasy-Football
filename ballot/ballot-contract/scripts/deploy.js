ae2dea0433c0db8fff5f602ba0a99116d1c33665
wesleycym <wesleycy@buffalo.edu>
2025-03-27 15:52:01 -0400

    Created Script

--- ballot/ballot-contract/scripts/deploy.js
+++ ballot/ballot-contract/scripts/deploy.js
@@ -1,1 +1,12 @@
 // NFT will be here to mint
+const hre = require("hardhat");
+async function main() {
+    const Demo = await hre.ethers.getContractFactory("storage");
+    const demo = await Demo.deploy("Hello, Hardhat!");
+    await demo.deployed();
+    console.log(`Contract deployed to: ${demo.address}`);
+}
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
