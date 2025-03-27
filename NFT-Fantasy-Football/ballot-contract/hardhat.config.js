de3ac21d64bfc0c237e7289a1e3ef76da1f3674b
wesleycym <wesleycy@buffalo.edu>
2025-03-27 17:08:03 -0400

    Project compiled

--- NFT-Fantasy-Football/ballot-contract/hardhat.config.js
+++ NFT-Fantasy-Football/ballot-contract/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -8,8 +9,8 @@ module.exports = {
       url: "http://127.0.0.1:8545",
     },
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-    accounts: [`0x${PRIVATE_KEY}`],
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
   },
 };
