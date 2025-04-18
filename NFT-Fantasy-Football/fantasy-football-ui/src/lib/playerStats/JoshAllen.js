6a114a40900c8f02a3fff2aa364b6ff316f98e6c
wesleycym <wesleycy@buffalo.edu>
2025-04-18 18:16:03 -0400

    Finished Josh Allens stats

--- NFT-Fantasy-Football/fantasy-football-ui/src/lib/playerStats/JoshAllen.js
+++ NFT-Fantasy-Football/fantasy-football-ui/src/lib/playerStats/JoshAllen.js
@@ -2,53 +2,51 @@
 
 // Remember to filter out bye weeks
 
-/* 1 point per 25 yards */
+/* 1 point per 25 yards {DONE} */
 export const PassingYards = 
 {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
-    11: 0,
-    12: 0,
-    13: 0,
-    14: 0,
-    15: 0,
-    16: 0,
-    17: 0,
-    18: 0
+    1: 232,
+    2: 139,
+    3: 263,
+    4: 180,
+    5: 131,
+    6: 215,
+    7: 323,
+    8: 283,
+    9: 235,
+    10: 280,
+    11: 262,
+    13: 148,
+    14: 342,
+    15: 362,
+    16: 154,
+    17: 182,
+    18: 0 // Allen played one snap in week 18
 };
 
-/* 4 points per */
+/* 4 points per {DONE} */
 export const PassingTouchdowns = 
 {
-    1: 0,
-    2: 0,
-    3: 0,
+    1: 2,
+    2: 1,
+    3: 4,
     4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
+    5: 1,
+    6: 2,
+    7: 2,
+    8: 2,
+    9: 3,
     10: 0,
-    11: 0,
-    12: 0,
-    13: 0,
-    14: 0,
-    15: 0,
-    16: 0,
-    17: 0,
+    11: 1,
+    13: 2,
+    14: 3,
+    15: 2,
+    16: 1,
+    17: 2,
     18: 0
 };
 
-/* -2 points per */
+/* -2 points per {DONE} */
 export const PassingInterceptions = 
 {
     1: 0,
@@ -58,66 +56,63 @@ export const PassingInterceptions =
     5: 0,
     6: 0,
     7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
-    11: 0,
-    12: 0,
+    8: 1,
+    9: 1,
+    10: 2,
+    11: 1,
     13: 0,
     14: 0,
     15: 0,
-    16: 0,
+    16: 1,
     17: 0,
     18: 0
 };
 
-/* 1 point per 10 yards */
+/* 1 point per 10 yards {DONE} */
 export const RushingYards = 
 {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
-    11: 0,
-    12: 0,
-    13: 0,
-    14: 0,
-    15: 0,
-    16: 0,
-    17: 0,
+    1: 39,
+    2: 2,
+    3: 44,
+    4: 21,
+    5: 54,
+    6: 18,
+    7: 1,
+    8: 25,
+    9: 7,
+    10: 50,
+    11: 55,
+    13: 18,
+    14: 82,
+    15: 68,
+    16: 30,
+    17: 17,
     18: 0
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const RushingTouchdowns = 
 {
-    1: 0,
+    1: 2,
     2: 0,
     3: 0,
     4: 0,
     5: 0,
-    6: 0,
+    6: 1,
     7: 0,
     8: 0,
     9: 0,
-    10: 0,
-    11: 0,
-    12: 0,
-    13: 0,
-    14: 0,
-    15: 0,
+    10: 1,
+    11: 1,
+    13: 1,
+    14: 3,
+    15: 2,
     16: 0,
-    17: 0,
+    17: 1,
     18: 0
 };
 
-/* 1 point per*/
+/* 1 point per {DONE} */
 export const Receptions = 
 {
     1: 0,
@@ -131,7 +126,6 @@ export const Receptions =
     9: 0,
     10: 0,
     11: 0,
-    12: 0,
     13: 0,
     14: 0,
     15: 0,
@@ -140,7 +134,7 @@ export const Receptions =
     18: 0
 };
 
-/* 1 point per 10 yards */
+/* 1 point per 10 yards {DONE} */
 export const ReceivingYards = 
 {
     1: 0,
@@ -154,8 +148,7 @@ export const ReceivingYards =
     9: 0,
     10: 0,
     11: 0,
-    12: 0,
-    13: 0,
+    13: 7,
     14: 0,
     15: 0,
     16: 0,
@@ -163,7 +156,7 @@ export const ReceivingYards =
     18: 0
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const ReceivingTouchdowns = 
 {
     1: 0,
@@ -177,8 +170,7 @@ export const ReceivingTouchdowns =
     9: 0,
     10: 0,
     11: 0,
-    12: 0,
-    13: 0,
+    13: 1,
     14: 0,
     15: 0,
     16: 0,
@@ -186,7 +178,7 @@ export const ReceivingTouchdowns =
     18: 0
 };
 
-/* 2 points per */
+/* 2 points per {DONE} */
 export const TwoPointConversions = 
 {
     1: 0,
@@ -200,7 +192,6 @@ export const TwoPointConversions =
     9: 0,
     10: 0,
     11: 0,
-    12: 0,
     13: 0,
     14: 0,
     15: 0,
@@ -209,13 +200,13 @@ export const TwoPointConversions =
     18: 0
 };
 
-/* -2 points per */
+/* -2 points per {DONE} */
 export const FumblesLost = 
 {
-    1: 0,
+    1: 1,
     2: 0,
     3: 0,
-    4: 0,
+    4: 1,
     5: 0,
     6: 0,
     7: 0,
@@ -223,7 +214,6 @@ export const FumblesLost =
     9: 0,
     10: 0,
     11: 0,
-    12: 0,
     13: 0,
     14: 0,
     15: 0,
@@ -232,7 +222,7 @@ export const FumblesLost =
     18: 0
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const FumbleRecoveredForATouchdown = 
 {
     1: 0,
@@ -246,7 +236,6 @@ export const FumbleRecoveredForATouchdown =
     9: 0,
     10: 0,
     11: 0,
-    12: 0,
     13: 0,
     14: 0,
     15: 0,
