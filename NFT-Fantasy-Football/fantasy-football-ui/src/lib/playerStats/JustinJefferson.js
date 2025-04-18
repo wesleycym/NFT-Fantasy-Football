124ca4808b2461f3a79060dd134bd1a5f0abbf1d
wesleycym <wesleycy@buffalo.edu>
2025-04-18 18:59:57 -0400

    JJettas stats

--- NFT-Fantasy-Football/fantasy-football-ui/src/lib/playerStats/JustinJefferson.js
+++ NFT-Fantasy-Football/fantasy-football-ui/src/lib/playerStats/JustinJefferson.js
@@ -2,7 +2,7 @@
 
 // Remember to filter out bye weeks
 
-/* 1 point per 25 yards */
+/* 1 point per 25 yards {DONE} */
 export const PassingYards = 
 {
     1: 0,
@@ -10,10 +10,9 @@ export const PassingYards =
     3: 0,
     4: 0,
     5: 0,
-    6: 0,
     7: 0,
     8: 0,
-    9: 0,
+    9: 22,
     10: 0,
     11: 0,
     12: 0,
@@ -25,7 +24,7 @@ export const PassingYards =
     18: 0
 };
 
-/* 4 points per */
+/* 4 points per {DONE} */
 export const PassingTouchdowns = 
 {
     1: 0,
@@ -33,7 +32,6 @@ export const PassingTouchdowns =
     3: 0,
     4: 0,
     5: 0,
-    6: 0,
     7: 0,
     8: 0,
     9: 0,
@@ -48,7 +46,7 @@ export const PassingTouchdowns =
     18: 0
 };
 
-/* -2 points per */
+/* -2 points per {DONE} */
 export const PassingInterceptions = 
 {
     1: 0,
@@ -56,7 +54,6 @@ export const PassingInterceptions =
     3: 0,
     4: 0,
     5: 0,
-    6: 0,
     7: 0,
     8: 0,
     9: 0,
@@ -71,7 +68,7 @@ export const PassingInterceptions =
     18: 0
 };
 
-/* 1 point per 10 yards */
+/* 1 point per 10 yards {DONE} */
 export const RushingYards = 
 {
     1: 0,
@@ -79,8 +76,7 @@ export const RushingYards =
     3: 0,
     4: 0,
     5: 0,
-    6: 0,
-    7: 0,
+    7: 3,
     8: 0,
     9: 0,
     10: 0,
@@ -94,7 +90,7 @@ export const RushingYards =
     18: 0
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const RushingTouchdowns = 
 {
     1: 0,
@@ -102,7 +98,6 @@ export const RushingTouchdowns =
     3: 0,
     4: 0,
     5: 0,
-    6: 0,
     7: 0,
     8: 0,
     9: 0,
@@ -117,76 +112,73 @@ export const RushingTouchdowns =
     18: 0
 };
 
-/* 1 point per*/
+/* 1 point per {DONE} */
 export const Receptions = 
 {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
-    11: 0,
-    12: 0,
-    13: 0,
-    14: 0,
-    15: 0,
-    16: 0,
-    17: 0,
-    18: 0
+    1: 4,
+    2: 4,
+    3: 6,
+    4: 6,
+    5: 6,
+    7: 7,
+    8: 8,
+    9: 7,
+    10: 5,
+    11: 6,
+    12: 2,
+    13: 7,
+    14: 7,
+    15: 7,
+    16: 10,
+    17: 8,
+    18: 3
 };
 
-/* 1 point per 10 yards */
+/* 1 point per 10 yards {DONE} */
 export const ReceivingYards = 
 {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
-    11: 0,
-    12: 0,
-    13: 0,
-    14: 0,
-    15: 0,
-    16: 0,
-    17: 0,
-    18: 0
+    1: 59,
+    2: 133,
+    3: 81,
+    4: 85,
+    5: 92,
+    7: 81,
+    8: 115,
+    9: 137,
+    10: 48,
+    11: 81,
+    12: 27,
+    13: 99,
+    14: 132,
+    15: 73,
+    16: 144,
+    17: 92,
+    18: 54
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const ReceivingTouchdowns = 
 {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
+    1: 1,
+    2: 1,
+    3: 1,
+    4: 1,
     5: 0,
-    6: 0,
-    7: 0,
+    7: 1,
     8: 0,
     9: 0,
     10: 0,
     11: 0,
     12: 0,
     13: 0,
-    14: 0,
-    15: 0,
-    16: 0,
+    14: 2,
+    15: 1,
+    16: 2,
     17: 0,
     18: 0
 };
 
-/* 2 points per */
+/* 2 points per {DONE} */
 export const TwoPointConversions = 
 {
     1: 0,
@@ -194,7 +186,6 @@ export const TwoPointConversions =
     3: 0,
     4: 0,
     5: 0,
-    6: 0,
     7: 0,
     8: 0,
     9: 0,
@@ -209,7 +200,7 @@ export const TwoPointConversions =
     18: 0
 };
 
-/* -2 points per */
+/* -2 points per {DONE} */
 export const FumblesLost = 
 {
     1: 0,
@@ -217,7 +208,6 @@ export const FumblesLost =
     3: 0,
     4: 0,
     5: 0,
-    6: 0,
     7: 0,
     8: 0,
     9: 0,
@@ -232,7 +222,7 @@ export const FumblesLost =
     18: 0
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const FumbleRecoveredForATouchdown = 
 {
     1: 0,
@@ -240,7 +230,6 @@ export const FumbleRecoveredForATouchdown =
     3: 0,
     4: 0,
     5: 0,
-    6: 0,
     7: 0,
     8: 0,
     9: 0,
