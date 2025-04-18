322c0ef75c63ca23f51bfd9f8477024564f11596
wesleycym <wesleycy@buffalo.edu>
2025-04-18 19:08:46 -0400

    Malik Nabers completed

--- NFT-Fantasy-Football/fantasy-football-ui/src/lib/playerStats/MalikNabers.js
+++ NFT-Fantasy-Football/fantasy-football-ui/src/lib/playerStats/MalikNabers.js
@@ -2,7 +2,7 @@
 
 // Remember to filter out bye weeks
 
-/* 1 point per 25 yards */
+/* 1 point per 25 yards {DONE} */
 export const PassingYards = 
 {
     1: 0,
@@ -15,7 +15,6 @@ export const PassingYards =
     8: 0,
     9: 0,
     10: 0,
-    11: 0,
     12: 0,
     13: 0,
     14: 0,
@@ -25,7 +24,7 @@ export const PassingYards =
     18: 0
 };
 
-/* 4 points per */
+/* 4 points per {DONE} */
 export const PassingTouchdowns = 
 {
     1: 0,
@@ -38,7 +37,6 @@ export const PassingTouchdowns =
     8: 0,
     9: 0,
     10: 0,
-    11: 0,
     12: 0,
     13: 0,
     14: 0,
@@ -48,7 +46,7 @@ export const PassingTouchdowns =
     18: 0
 };
 
-/* -2 points per */
+/* -2 points per {DONE} */
 export const PassingInterceptions = 
 {
     1: 0,
@@ -61,7 +59,6 @@ export const PassingInterceptions =
     8: 0,
     9: 0,
     10: 0,
-    11: 0,
     12: 0,
     13: 0,
     14: 0,
@@ -71,22 +68,21 @@ export const PassingInterceptions =
     18: 0
 };
 
-/* 1 point per 10 yards */
+/* 1 point per 10 yards {DONE} */
 export const RushingYards = 
 {
     1: 0,
     2: 0,
-    3: 0,
-    4: 0,
+    3: 4,
+    4: -4,
     5: 0,
     6: 0,
     7: 0,
     8: 0,
     9: 0,
-    10: 0,
-    11: 0,
+    10: -2,
     12: 0,
-    13: 0,
+    13: 4,
     14: 0,
     15: 0,
     16: 0,
@@ -94,7 +90,7 @@ export const RushingYards =
     18: 0
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const RushingTouchdowns = 
 {
     1: 0,
@@ -107,7 +103,6 @@ export const RushingTouchdowns =
     8: 0,
     9: 0,
     10: 0,
-    11: 0,
     12: 0,
     13: 0,
     14: 0,
@@ -117,58 +112,56 @@ export const RushingTouchdowns =
     18: 0
 };
 
-/* 1 point per*/
+/* 1 point per {DONE} */
 export const Receptions = 
 {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
+    1: 5,
+    2: 10,
+    3: 8,
+    4: 12,
     5: 0,
     6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
-    11: 0,
-    12: 0,
-    13: 0,
-    14: 0,
-    15: 0,
-    16: 0,
-    17: 0,
-    18: 0
+    7: 4,
+    8: 7,
+    9: 9,
+    10: 6,
+    12: 6,
+    13: 8,
+    14: 5,
+    15: 10,
+    16: 7,
+    17: 7,
+    18: 5
 };
 
-/* 1 point per 10 yards */
+/* 1 point per 10 yards {DONE} */
 export const ReceivingYards = 
 {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
+    1: 66,
+    2: 127,
+    3: 78,
+    4: 115,
     5: 0,
     6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
-    11: 0,
-    12: 0,
-    13: 0,
-    14: 0,
-    15: 0,
-    16: 0,
-    17: 0,
-    18: 0
+    7: 41,
+    8: 71,
+    9: 59,
+    10: 50,
+    12: 64,
+    13: 69,
+    14: 79,
+    15: 82,
+    16: 68,
+    17: 171,
+    18: 64
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const ReceivingTouchdowns = 
 {
     1: 0,
-    2: 0,
-    3: 0,
+    2: 1,
+    3: 2,
     4: 0,
     5: 0,
     6: 0,
@@ -176,14 +169,13 @@ export const ReceivingTouchdowns =
     8: 0,
     9: 0,
     10: 0,
-    11: 0,
     12: 0,
     13: 0,
     14: 0,
-    15: 0,
+    15: 1,
     16: 0,
-    17: 0,
-    18: 0
+    17: 2,
+    18: 1
 };
 
 /* 2 points per */
@@ -199,10 +191,9 @@ export const TwoPointConversions =
     8: 0,
     9: 0,
     10: 0,
-    11: 0,
     12: 0,
     13: 0,
-    14: 0,
+    14: 1,
     15: 0,
     16: 0,
     17: 0,
@@ -222,7 +213,6 @@ export const FumblesLost =
     8: 0,
     9: 0,
     10: 0,
-    11: 0,
     12: 0,
     13: 0,
     14: 0,
@@ -245,7 +235,6 @@ export const FumbleRecoveredForATouchdown =
     8: 0,
     9: 0,
     10: 0,
-    11: 0,
     12: 0,
     13: 0,
     14: 0,
