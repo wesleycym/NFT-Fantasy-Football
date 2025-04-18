4967c01e481a925a69c75fcb1cc3a1322bd5a79f
wesleycym <wesleycy@buffalo.edu>
2025-04-17 21:38:34 -0400

    Created template for storing fantasy points

---
+++ NFT-Fantasy-Football/fantasy-football-ui/src/lib/playerStats/JoshAllen.js
@@ -0,0 +1,45 @@
+/* Josh Allens 2024-2025 season stats */
+
+/* 1 point per 25 yards */
+export const PassingYards = 
+{};
+
+/* 4 points per */
+export const PassingTouchdowns = 
+{};
+
+/* -2 points per */
+export const PassingInterceptions = 
+{};
+
+/* 1 point per 10 yards */
+export const RushingYards = 
+{};
+
+/* 6 points per */
+export const RushingTouchdowns = 
+{};
+
+/* 1 point per*/
+export const Receptions = 
+{};
+
+/* 1 point per 10 yards */
+export const ReceivingYards = 
+{};
+
+/* 6 points per */
+export const ReceivingTouchdowns = 
+{};
+
+/* 2 points per */
+export const TwoPointConversions = 
+{};
+
+/* -2 points per */
+export const FumblesLost = 
+{};
+
+/* 6 points per */
+export const FumbleRecoveredForATouchdown = 
+{};
