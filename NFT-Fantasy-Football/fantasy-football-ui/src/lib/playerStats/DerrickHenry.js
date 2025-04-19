f9bed1834c00207579754e7023683e358c5a57d9
wesleycym <wesleycy@buffalo.edu>
2025-04-19 16:20:35 -0400

    Finished derrick henrys stats; Should verify them for errors

--- NFT-Fantasy-Football/fantasy-football-ui/src/lib/playerStats/DerrickHenry.js
+++ NFT-Fantasy-Football/fantasy-football-ui/src/lib/playerStats/DerrickHenry.js
@@ -2,7 +2,7 @@
 
 // Remember to filter out bye weeks
 
-/* 1 point per 25 yards */
+/* 1 point per 25 yards {DONE} */
 export const PassingYards = 
 {
     1: 0,
@@ -18,14 +18,13 @@ export const PassingYards =
     11: 0,
     12: 0,
     13: 0,
-    14: 0,
     15: 0,
     16: 0,
     17: 0,
     18: 0
 };
 
-/* 4 points per */
+/* 4 points per {DONE} */
 export const PassingTouchdowns = 
 {
     1: 0,
@@ -41,14 +40,13 @@ export const PassingTouchdowns =
     11: 0,
     12: 0,
     13: 0,
-    14: 0,
     15: 0,
     16: 0,
     17: 0,
     18: 0
 };
 
-/* -2 points per */
+/* -2 points per {DONE} */
 export const PassingInterceptions = 
 {
     1: 0,
@@ -64,129 +62,123 @@ export const PassingInterceptions =
     11: 0,
     12: 0,
     13: 0,
-    14: 0,
     15: 0,
     16: 0,
     17: 0,
     18: 0
 };
 
-/* 1 point per 10 yards */
+/* 1 point per 10 yards {DONE} */
 export const RushingYards = 
 {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
-    11: 0,
-    12: 0,
-    13: 0,
-    14: 0,
-    15: 0,
-    16: 0,
-    17: 0,
-    18: 0
+    1: 46,
+    2: 84,
+    3: 151,
+    4: 199,
+    5: 92,
+    6: 132,
+    7: 169,
+    8: 73,
+    9: 106,
+    10: 68,
+    11: 65,
+    12: 140,
+    13: 82,
+    15: 67,
+    16: 162,
+    17: 147,
+    18: 138
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const RushingTouchdowns = 
 {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
+    1: 1,
+    2: 1,
+    3: 2,
+    4: 1,
+    5: 1,
+    6: 2,
     7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
-    11: 0,
+    8: 1,
+    9: 2,
+    10: 1,
+    11: 1,
     12: 0,
     13: 0,
-    14: 0,
     15: 0,
     16: 0,
-    17: 0,
-    18: 0
+    17: 1,
+    18: 2
 };
 
-/* 1 point per*/
+/* 1 point per {DONE} */
 export const Receptions = 
 {
     1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
+    2: 1,
+    3: 1,
+    4: 3,
+    5: 1,
     6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
+    7: 1,
+    8: 1,
+    9: 1,
+    10: 1,
     11: 0,
     12: 0,
-    13: 0,
-    14: 0,
+    13: 3,
     15: 0,
-    16: 0,
-    17: 0,
-    18: 0
+    16: 2,
+    17: 2,
+    18: 2
 };
 
-/* 1 point per 10 yards */
+/* 1 point per 10 yards {DONE} */
 export const ReceivingYards = 
 {
     1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
+    2: 12,
+    3: 23,
+    4: 10,
+    5: 4,
     6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
+    7: 13,
+    8: 4,
+    9: 27,
+    10: 3,
     11: 0,
     12: 0,
-    13: 0,
-    14: 0,
+    13: 29,
     15: 0,
-    16: 0,
-    17: 0,
-    18: 0
+    16: 27,
+    17: 18,
+    18: 23
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const ReceivingTouchdowns = 
 {
     1: 0,
     2: 0,
     3: 0,
-    4: 0,
+    4: 1,
     5: 0,
     6: 0,
-    7: 0,
+    7: 1,
     8: 0,
     9: 0,
     10: 0,
     11: 0,
     12: 0,
     13: 0,
-    14: 0,
     15: 0,
     16: 0,
     17: 0,
     18: 0
 };
 
-/* 2 points per */
+/* 2 points per {DONE} */
 export const TwoPointConversions = 
 {
     1: 0,
@@ -202,14 +194,13 @@ export const TwoPointConversions =
     11: 0,
     12: 0,
     13: 0,
-    14: 0,
     15: 0,
     16: 0,
     17: 0,
     18: 0
 };
 
-/* -2 points per */
+/* -2 points per {DONE} */
 export const FumblesLost = 
 {
     1: 0,
@@ -222,17 +213,16 @@ export const FumblesLost =
     8: 0,
     9: 0,
     10: 0,
-    11: 0,
+    11: 1,
     12: 0,
     13: 0,
-    14: 0,
     15: 0,
     16: 0,
     17: 0,
     18: 0
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const FumbleRecoveredForATouchdown = 
 {
     1: 0,
@@ -248,7 +238,6 @@ export const FumbleRecoveredForATouchdown =
     11: 0,
     12: 0,
     13: 0,
-    14: 0,
     15: 0,
     16: 0,
     17: 0,
