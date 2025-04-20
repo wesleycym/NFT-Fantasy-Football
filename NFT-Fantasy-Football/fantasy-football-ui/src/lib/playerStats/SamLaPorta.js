c5e44f7454d84908ab5a343f057baeb057f29ac7
wesleycym <wesleycy@buffalo.edu>
2025-04-20 14:16:51 -0400

    Completed sam laporta stats

--- NFT-Fantasy-Football/fantasy-football-ui/src/lib/playerStats/SamLaPorta.js
+++ NFT-Fantasy-Football/fantasy-football-ui/src/lib/playerStats/SamLaPorta.js
@@ -2,7 +2,7 @@
 
 // Remember to filter out bye weeks
 
-/* 1 point per 25 yards */
+/* 1 point per 25 yards {DONE} */
 export const PassingYards = 
 {
     1: 0,
@@ -24,7 +24,7 @@ export const PassingYards =
     18: 0
 };
 
-/* 4 points per */
+/* 4 points per {DONE} */
 export const PassingTouchdowns = 
 {
     1: 0,
@@ -46,7 +46,7 @@ export const PassingTouchdowns =
     18: 0
 };
 
-/* -2 points per */
+/* -2 points per {DONE} */
 export const PassingInterceptions = 
 {
     1: 0,
@@ -68,7 +68,7 @@ export const PassingInterceptions =
     18: 0
 };
 
-/* 1 point per 10 yards */
+/* 1 point per 10 yards {DONE} */
 export const RushingYards = 
 {
     1: 0,
@@ -90,7 +90,7 @@ export const RushingYards =
     18: 0
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const RushingTouchdowns = 
 {
     1: 0,
@@ -112,73 +112,73 @@ export const RushingTouchdowns =
     18: 0
 };
 
-/* 1 point per*/
+/* 1 point per {DONE} */
 export const Receptions = 
 {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
+    1: 4,
+    2: 2,
+    3: 2,
+    4: 4,
+    6: 1,
+    7: 1,
+    8: 6,
+    9: 2,
+    10: 3,
     11: 0,
-    12: 0,
-    13: 0,
-    14: 0,
-    15: 0,
-    16: 0,
-    17: 0,
-    18: 0
+    12: 3,
+    13: 3,
+    14: 5,
+    15: 7,
+    16: 4,
+    17: 6,
+    18: 7
 };
 
-/* 1 point per 10 yards */
+/* 1 point per 10 yards {DONE} */
 export const ReceivingYards = 
 {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
+    1: 45,
+    2: 13,
+    3: 36,
+    4: 53,
+    6: 52,
+    7: 25,
+    8: 48,
+    9: 28,
+    10: 66,
     11: 0,
-    12: 0,
-    13: 0,
-    14: 0,
-    15: 0,
-    16: 0,
-    17: 0,
-    18: 0
+    12: 19,
+    13: 6,
+    14: 54,
+    15: 111,
+    16: 43,
+    17: 64,
+    18: 63
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const ReceivingTouchdowns = 
 {
     1: 0,
     2: 0,
     3: 0,
     4: 0,
-    6: 0,
+    6: 1,
     7: 0,
-    8: 0,
+    8: 1,
     9: 0,
-    10: 0,
+    10: 1,
     11: 0,
     12: 0,
-    13: 0,
+    13: 2,
     14: 0,
     15: 0,
-    16: 0,
-    17: 0,
+    16: 1,
+    17: 1,
     18: 0
 };
 
-/* 2 points per */
+/* 2 points per {DONE} */
 export const TwoPointConversions = 
 {
     1: 0,
@@ -200,7 +200,7 @@ export const TwoPointConversions =
     18: 0
 };
 
-/* -2 points per */
+/* -2 points per {DONE} */
 export const FumblesLost = 
 {
     1: 0,
@@ -222,7 +222,7 @@ export const FumblesLost =
     18: 0
 };
 
-/* 6 points per */
+/* 6 points per {DONE} */
 export const FumbleRecoveredForATouchdown = 
 {
     1: 0,
