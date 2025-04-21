7e3485dd8c02d4333eb46ba7f483e78512525a9f
wesleycym <wesleycy@buffalo.edu>
2025-04-21 18:48:02 -0400

    Created an available players list

---
+++ NFT-Fantasy-Football/fantasy-football-ui/src/lib/AvailablePlayers.js
@@ -0,0 +1,50 @@
+export const mintablePlayers = [
+    {
+      name: "Josh Allen",
+      position: "QB",
+      team: "Buffalo Bills",
+      mintPrice: "50", // in Yoda tokens
+    },
+    {
+      name: "Patrick Mahomes",
+      position: "QB",
+      team: "Kansas City Chiefs",
+      mintPrice: "50", // in Yoda tokens
+    },
+    {
+      name: "Justin Jefferson",
+      position: "WR",
+      team: "Minnesota Vikings",
+      mintPrice: "50", // in Yoda tokens
+    },
+    {
+      name: "Malik Nabers",
+      position: "WR",
+      team: "New York Giants",
+      mintPrice: "50", // in Yoda tokens
+    },
+    {
+      name: "Saquan Barkley",
+      position: "RB",
+      team: "Philadelphia Eagles",
+      mintPrice: "50", // in Yoda tokens
+    },
+    {
+        name: "Derrick Henry",
+        position: "RB",
+        team: "Baltimore Ravens",
+        mintPrice: "50", // in Yoda tokens
+    },
+    {
+        name: "Brock Bowers",
+        position: "TE",
+        team: "Las Vegas Raiders",
+        mintPrice: "50",
+    },
+    {
+        name: "Sam LaPorta",
+        position: "TE",
+        team: "Detroit Lions",
+        mintPrice: "50",
+    }
+];
