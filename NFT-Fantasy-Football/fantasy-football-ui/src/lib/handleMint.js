55c58b316ed8a6c8ca1a21181832bc995a0710fe
wesleycym <wesleycy@buffalo.edu>
2025-04-22 15:34:56 -0400

    Sending preformatted description to mint function; Will update contract soon

--- NFT-Fantasy-Football/fantasy-football-ui/src/lib/handleMint.js
+++ NFT-Fantasy-Football/fantasy-football-ui/src/lib/handleMint.js
@@ -14,7 +14,15 @@ export async function handleMint(player, contractAddress, setIsBuying) {
     const mintPrice = ethers.parseUnits(player.mintPrice.toString(), 18);
     // const breakdownString = breakdown.join(" | ");
 
-    const breakdownString = breakdown.map(item => `- ${item}`).join("%0A"); // Each breakdown item on a new line
+    const breakdownString = breakdown.map(item => `- ${item}`).join("\\n"); // Each breakdown item on a new line
+
+    // Pre formatting description
+    const description = [
+        `${player.name} - ${player.position} for ${player.team}`,
+        `🏈 Fantasy Points: ${fantasyPoints}`,
+        `⭐ Rank: ${rank}`,
+        `📊 Breakdown:\n${breakdownString}`
+      ].join("\\n");
 
     const tx = await contract.mint(
       await signer.getAddress(),
@@ -25,7 +33,7 @@ export async function handleMint(player, contractAddress, setIsBuying) {
       mintPrice,
       false,
       0,
-      breakdownString,
+      description,
       rank
     );
 
