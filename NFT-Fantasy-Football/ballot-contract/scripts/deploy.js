83bcb7433e9fab322f1b808d3f8be73e7cb64d04
wesleycym <wesleycy@buffalo.edu>
2025-04-26 14:48:32 -0400

    Revamped deploy contract for testing

--- NFT-Fantasy-Football/ballot-contract/scripts/deploy.js
+++ NFT-Fantasy-Football/ballot-contract/scripts/deploy.js
@@ -1,25 +1,45 @@
 // NFT will be here to mint
 const hre = require("hardhat");
 
-async function main() {
+const LOCAL_TESTING = true; // constant should be uppercase but assigned properly
 
+async function main() {
     const [deployer] = await hre.ethers.getSigners(); // Get the deployer account
 
-    const FantasyFootball = await hre.ethers.getContractFactory("FantasyFootball"); // Preparing the contract
+    let yodaAddress; // Declare yoda address
 
-    const contract = await FantasyFootball.deploy(
-        "0xe1d6e2F8F036179656bEb0E2BDb8E326b0E6b094", // YODA token address
-        "FantasyFootball", // Name of collection
-        "FFNFT", // Symbol of collection
-        1 // Global mint price in YODA (set to 1 to test transfers)
+    if (LOCAL_TESTING) {
+        console.log("Local Testing");
+
+        const YODA = await hre.ethers.getContractFactory("YODA");
+        const yoda = await YODA.deploy("Yoda Token", "YODA", 1000000); // 1 million supply
+        await yoda.deploymentTransaction().wait();
+        
+        yodaAddress = yoda.target;
+
+        console.log("YODA deployed to:", yodaAddress);
+
+    } else {
+        console.log("Sepolia deployment");
+
+        yodaAddress = "0xe1d6e2F8F036179656bEb0E2BDb8E326b0E6b094"; // Given yoda address
+    }
+
+    console.log("Deploying FantasyFootball contract");
+
+    const FantasyFootball = await hre.ethers.getContractFactory("FantasyFootball");
+    
+    const fantasyFootball = await FantasyFootball.deploy(
+        yodaAddress, // YODA address
+        "FantasyFootball", // NFT name
+        "FFNFT", // NFT symbol
+        1 // Global mint price (1 YODA)
     );
 
-    //await contract.waitForDeployment(); // Wait for the contract to be deployed
-    await contract.deploymentTransaction().wait(); // Forcefully waits for the transaction to confirm
+    await fantasyFootball.deploymentTransaction().wait();
 
-    console.log("FantasyFootball NFT contract deployed to:", contract.target); // Debug
+    console.log("FantasyFootball contract deployed to:", fantasyFootball.target);
 
-    // Debug
     console.log("Deployer address:", deployer.address);
 }
 
